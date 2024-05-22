6060095fdccd0a71532bafdfa24a2290fdc31cb3
Kayla Annaya <kayannaya@Kaylas-MacBook-Pro-2.local>
2024-05-22 17:06:39 +0700

    Kumpul Rev 1

--- finpro.c
+++ finpro.c
@@ -29,7 +29,21 @@ void menu(){
 void input_Nama(){
 
 
+}
 
+void input_Usia(){
+
+
+}
+void input_Tinggi(){
+
+
+}
+void input_Berat(){
+
+
+}
+void input_GD(){
 
 
 }
@@ -43,4 +57,33 @@ float BMI(int height, int weight){
     return bmi;
 }
 
-void 
+void BMI_Level(){
+
+
+}
+
+void GD_Level(){
+
+
+}
+
+void accessPatient(){
+
+
+}
+
+void accessCat(){
+
+
+
+}
+
+void exit(){
+
+    printf("Thank you for using our program. Stay healthy and have a nice day!");
+    break;
+    
+}
+
+
+
