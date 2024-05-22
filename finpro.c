2acf4f266cb2bdb2210d47590c347a4bec1aebf1
Kayla Annaya <kayannaya@Kaylas-MacBook-Pro-2.local>
2024-05-22 16:47:29 +0700

    Kumpul

--- finpro.c
+++ finpro.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
 
+typedef struct data{
+    char namaPasien[100];
+    float height;
+    float weight;
+    int 
+} data;
+
+int main (){
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+void menu(){
+    int 
+
+
+
+}
+
+void input_Nama(){
+
+
+
+
+
+}
+
+float BMI(int height, int weight){
+
+    float bmi;
+
+    bmi = height/(weight^2);
+
+    return bmi;
+}
+
+void 
