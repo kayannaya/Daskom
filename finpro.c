ec90a1c1f061197d7b9a2d554aadaabf04928f62
Kayla Annaya <kayannaya@Kaylas-MacBook-Pro-2.local>
2024-06-02 15:30:49 +0700

    Update 2 Juni 2024

--- finpro.c
+++ finpro.c
@@ -6,16 +6,63 @@ typedef struct data{
     float berat;
     float tinggi;
     int gd;
-    int stat[100];
+    char stat[100];
 } data;
 
 
 int main (){
 
-    data pasien[i];
+    int i, d, 
+    int m = 1;
 
+    struct data Data[i];
 
+    printf("Welcome to DiaBase, a diabetes risk calculation and database!\n");
 
+    while (m == 1){
+    
+        printf("How can we help you today?");
+
+        printf("1. Input data\n");
+        printf("2. Access data\n");
+        printf("3. Exit program\n");
+
+        printf("Enter your option : ");
+        scanf("%d", &d);
+
+        if (d == 1) {
+
+            //Data input
+            input_Data();
+
+            //Parameter calculation
+            BMI_Level();
+            GD_Level();
+
+            //Risk calculation
+            Status;
+        }
+
+        else if (d == 2)
+        {
+            accessPatient();
+        }
+
+        else if (d == 3)
+        {
+            printf("Thank you for using our Diabase!\n");
+
+            //To make the while loop stops
+            m = 0;
+        }
+
+        else
+        {
+            printf("Invalid option. Please enter the correct option from 1-3.\n");
+        }
+        
+    }
+    
     return 0;
 }
 
@@ -23,6 +70,8 @@ void menu(){
     int 
 
 
+    printf("Thank you for using our program. Stay healthy and have a nice day!");
+    break;
 
 }
 
@@ -30,21 +79,19 @@ void input_Data(){
 
     int n;
 
-    printf("Masukkan jumlah data yang ingin di-input");
+    printf("Masukkan jumlah pasien yang ingin di-input");
     scanf("%d", &n);
 
-    for(i = 0 ; i < n ; i++) {
+    for(i = 0 ; i < n ; i++){
 
-        printf("Pasien %d:\n", i+1);
+        printf("Nama pasien ke-%d: ", counter);
+        scanf(" %[^\n]s", Data[i].nama);
 
-        printf("Masukkan Nama : ");
-        scanf("%s", ILLIT.member[i].name);
+        printf("id buku ke-%d: ", counter);
+        scanf("%d", &Data[i].nomorBuku);
 
-        printf("Masukkan Umur : ");
-        scanf("%d", &ILLIT.member[i].age);
-
-        printf("Masukkan Posisi : ");
-        scanf("%s", ILLIT.member[i].position);
+        printf("Rating buku ke-%d: ", counter);
+        scanf("%d", &Data[i].rating);
 
         printf("\n");
 
@@ -62,33 +109,23 @@ float BMI(int height, int weight){
     return bmi;
 }
 
-void BMI_Level(){
+void BMI_Level(){ //Comparing BMI data with literature
 
 
 }
 
-void GD_Level(){
+void GD_Level(){ //Comparing GD data with literature
 
 
 }
 
-void accessPatient(){
-
-
-}
-
-void accessCat(){
+void Status(){ //Calculation diabetes or not + display data
 
 
 
 }
 
-void exit(){
+void accessPatient(){ //Access the data for each patient
 
-    printf("Thank you for using our program. Stay healthy and have a nice day!");
-    break;
-    
+
 }
-
-
-
